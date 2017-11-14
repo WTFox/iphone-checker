d2bcaf9fde0442f240edc4a054af62263a1a5e9f
Anthony Fox <AnthonyFox1988@gmail.com>
2017-11-14 12:15:41 -0800

    Cleanup API

--- iphone_checker.py
+++ iphone_checker.py
@@ -1,14 +1,26 @@
 import requests
 
 
+# TODO Add Click CLI
+# TODO Upload to pip
+# TODO Add notify options
+
 # Carrier
-cppart = 'TMOBILE/US'
+VALID_CARRIERS = [
+    'TMOBILE',
+    'ATT',
+    'SPRINT',
+    'VERIZON'
+]
 
 # Where to search
-zipcodes = ['92620', '92691']
+ZIPCODES = [
+    '92620',
+    '92691',
+]
 
 # What are we looking for?
-devices = {
+DEVICES = {
     # readable name,  parts.0
     '64GB Space Grey': 'MQAQ2LL/A',
     '256GB Space Grey': 'MQAU2LL/A',
@@ -16,44 +28,60 @@ devices = {
     '256GB Silver': 'MQAV2LL/A',
 }
 
-# No idea what this is...
-pl = True
+# Base URL
+URL = 'https://www.apple.com/shop/retail/pickup-message'
+PICKUP_AVAILABLE = 'available'
+CARRIER = 'TMOBILE'
 
-# base url
-url = 'https://www.apple.com/shop/retail/pickup-message'
 
-
-def do_search(zipcode, device):
-    response = requests.get(url, params={
-        'pl': pl,
-        'cppart': cppart,
+def search_stores_in_zipcode(zipcode, device):
+    stores_with_device = []
+    response = requests.get(URL, params={
+        'pl': True,
+        'cppart': CARRIER,
         'location': zipcode,
         'parts.0': device
     })
 
     for store in response.json().get('body', {}).get('stores', []):
-        if has_iphone(store, device):
-            print(format_store_display(store))
+        if store_has_device(store, device):
+            stores_with_device.append(store)
+
+    return stores_with_device
 
 
-def has_iphone(store, device):
-    status_dict = store.get('partsAvailability', {}).get(device, {})
-    if status_dict.get('pickupDisplay', '') == 'unavailable':
-        return False
-    else:
-        return True
+def store_has_device(store, device):
+    device_dict = store.get('partsAvailability', {}).get(device, {})
+    return device_dict.get('pickupDisplay', '') == PICKUP_AVAILABLE
 
 
 def format_store_display(store):
-    return '{}({}) has the iphone in stock. Check the url here\n{}\nor call them @ {}'.format(
+    device_name = store.get('partsAvailability', {}).values()[0].get('storePickupProductTitle')
+    return '{} ({}) has the {} in stock.\nCheck the url here:\n{}\n\nor call them @ {}'.format(
         store.get('storeName'),
         store.get('storeDistanceVoText'),
+        device_name,
         store.get('reservationUrl'),
         store.get('phoneNumber')
     )
 
 
+def main():
+    assert CARRIER in VALID_CARRIERS
+
+    total_results = []
+    for zipcode in ZIPCODES:
+        for name, device in DEVICES.items():
+            total_results.extend(search_stores_in_zipcode(zipcode, device))
+
+    if not total_results:
+        print("No stores near {} have stock. :(".format(
+            ', '.join(ZIPCODES),
+        ))
+    
+    for store in set(total_results):
+        print(format_store_display(store))
+
+
 if __name__ == '__main__':
-    for zipcode in zipcodes:
-        for name, device in devices.items():
-            do_search(zipcode, device)
+    main()
